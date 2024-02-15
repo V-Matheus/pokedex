166c0d96a2939e4176c18c91c050eeb683a53d83
Victor Matheus <victormatheus507@gmail.com>
2024-02-15 15:28:09 -0300

    criando modelo de pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,15 @@
-const offset = 0;
-const limit = 10;
-
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limite=${limit}`;
-
-function convertPokemonTypesToLi(pokemonTypes) {
-  return pokemonTypes.map(
-    (typeSlot) =>
-      `
-        <li class='type'>${typeSlot.type.name}</li>
-    `,
-  );
-}
-
 function convertPokemonToHtml(pokemon) {
   return `<li class="pokemon">
-  <span class="number">#${pokemon.order}</span>
+  <span class="number">#${pokemon.number}</span>
   <span class="name">${pokemon.name}</span>
 
   <div class="detail">
 
     <ol class="types">
-      ${convertPokemonTypesToLi(pokemon.types).join('')}
+      ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
     </ol>
 
-    <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${
-    pokemon.name
-  }">
+    <img src="${pokemon.photo}" alt="${pokemon.name}">
   </div>
 
 </li>`;
