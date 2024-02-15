3df56b6b82bdb44be8405a03a7b874db02e35c0b
Victor Matheus <victormatheus507@gmail.com>
2024-02-15 13:37:43 -0300

    primeira requisicao

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
 console.log('ok');
+
+const offset = 0;
+const limit = 10;
+
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limite=${limit}`;
+
+fetch(url).then((response) => console.log(response));
