51eed51d434f61a72c737e02f24a8d7f23611348
Victor Matheus <victormatheus507@gmail.com>
2024-02-15 13:46:58 -0300

    manipulando resultado

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,6 @@ const limit = 10;
 
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limite=${limit}`;
 
-fetch(url).then((response) => console.log(response));
+fetch(url).then((response) =>
+  response.json().then((json) => console.log(json)),
+);
