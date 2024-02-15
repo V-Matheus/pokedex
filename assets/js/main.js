0e8a9ebd1e442cf60a3396978f876c974e7731db
Victor Matheus <victormatheus507@gmail.com>
2024-02-15 14:07:01 -0300

    exibindo a lista de pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,33 @@ const limit = 10;
 
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limite=${limit}`;
 
-fetch(url).then((response) =>
-  response.json().then((json) => console.log(json)),
-);
+function convertPokemonToHtml(pokemon) {
+  return `<li class="pokemon">
+  <span class="number">#001</span>
+  <span class="name">${pokemon.name}</span>
+
+  <div class="detail">
+
+    <ol class="types">
+      <li class="type">Grass</li>
+      <li class="type">Poison</li>  
+    </ol>
+
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+  </div>
+
+</li>`;
+}
+
+const pokemonList = document.getElementById('pokemonList');
+
+
+fetch(url)
+  .then((response) => response.json())
+  .then((json) => json.results)
+  .then((pokemons) => {
+    for (let i = 0; i < pokemons.length; i++) {
+      const pokemon = pokemons[i];
+      pokemonList.innerHTML += convertPokemonToHtml(pokemon);
+    }
+  });
